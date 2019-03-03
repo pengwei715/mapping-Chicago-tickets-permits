542eb71e153bf04bec0f370bfd5df616799546bb
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-03 01:09:49 -0600

    write a piece of code to store data into csv

--- ticket_project/permits.py
+++ ticket_project/permits.py
@@ -19,23 +19,36 @@ import csv
 from sodapy import Socrata
 
 
+
 DATA_DIR = "./data/"
 
-def get_sample(num, filename):
-	'''
-	Get some sample permit data
-	Input:
-	    num: number of rows 
-	    filename: the name of the csv file 
-	Return:
-	    No return, the csv file contains the sample data.
-	'''
+def get_sample(num, filename = "permits_sample.csv"):
+    '''
+    Get some sample permit data
+    Input:
+        num: number of rows 
+        filename: the name of the csv file 
+    Return:
+        No return, the csv file contains the sample data.
+    '''
+    client = Socrata("data.cityofchicago.org", None)
 
-	# Unauthenticated client only works with public data sets. Note 'None'
-	# in place of application token, and no username or password:
-	client = Socrata("data.cityofchicago.org", None)
+    # First 2000 results, returned as JSON from API / converted to Python list of
+    # dictionaries by sodapy.
+    results = client.get("erhc-fkv9", limit=num)
 
-	# First 2000 results, returned as JSON from API / converted to Python list of
-	# dictionaries by sodapy.
-	results = client.get("erhc-fkv9", limit=num)
+    temp = set()
+    for item in results:
+        temp.update(set(item.keys()))
+    csv_columns = temp
+
+    csv_file = DATA_DIR + filename
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in results:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
 
