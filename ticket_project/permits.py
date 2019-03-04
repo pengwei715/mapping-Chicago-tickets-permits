ae839402c07a9e6fa34b9394387e3a39663636a2
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-03 23:17:35 -0600

    add trim function

--- ticket_project/permits.py
+++ ticket_project/permits.py
@@ -12,12 +12,11 @@ Find proper way interface with API and load in ata
 #requre the sodapy library
 import os
 import sys
-import pandas
+import pandas as pd
 import requests
 import json
 import csv
 from sodapy import Socrata
-
 
 
 DATA_DIR = "./data/"
@@ -32,14 +31,49 @@ def get_sample(num, filename = "permits_sample.csv"):
         No return, the csv file contains the sample data.
     '''
     client = Socrata("data.cityofchicago.org", None)
-
-    # First 2000 results, returned as JSON from API / converted to Python list of
-    # dictionaries by sodapy.
+    if num == -1:
+        num = None
     results = client.get("erhc-fkv9", limit=num)
+    write_csv(results, filename)
+
+def trim_data(input_filename, output_filename):
+    '''
+    Find trim the data keep the time and location infor
+    Input:
+        input_filename: the raw csv file
+        output_filename: name of the result file
+    Return:
+        No return, filter the data and store it to csv file
+    '''
+    raw = read_csv(input_filename)
+    useful = raw.loc[:,['uniquekey','applicationtype',
+        'applicationtype','applicationstartdate',
+        'worktype', 'worktypedescription',
+        'applicationfinalizeddate','parkingmeterpostingorbagging',
+        'xcoordinate', 'ycoordinate', 'latitude',
+        'longitude','location','streetclosure',
+        'streetnumberfrom','streetnumberto',
+        'direction','streetname']]
+    output = DATA_DIR + output_filename
+    useful.to_csv(output)
+
+
+def read_csv(filename):
+    '''
+    Read data from the file 
+    Return pd dataframe
+    '''
+
+    csv_file = DATA_DIR + filename
+    data = pd.read_csv(csv_file)
+    return data
+
+def write_csv(results,filename):
+
 
     temp = set()
-    for item in results:
-        temp.update(set(item.keys()))
+    for i in range(200):
+        temp.update(set(results[i].keys()))
     csv_columns = temp
 
     csv_file = DATA_DIR + filename
@@ -51,4 +85,3 @@ def get_sample(num, filename = "permits_sample.csv"):
                 writer.writerow(data)
     except IOError:
         print("I/O error")
-
