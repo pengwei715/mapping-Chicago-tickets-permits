76afd22ebbfc40d4fa924c0286315679ae3ac050
Ben Fogarty <benfogarty@cs.uchicago.edu>
2019-03-10 00:54:33 -0600

    Updated neighborhoods

--- ticket_project/neighborhoods.py
+++ ticket_project/neighborhoods.py
@@ -9,31 +9,33 @@ def read_shapefiles(filepath):
     Reads the neighborhood shapefiles into a geopandas dataframe
 
     Inputs:
-    	filepath (str): the path to the neighborhood shapefiles
+        filepath (str): the path to the neighborhood shapefiles
 
     Returns: geopandas dataframe describing neighborhoods
     '''
     return geo_pd.read_file(filepath)
 
 
-def convert_to_geodf(df, proj):
-	'''
-	Converts a regular pandas dataframe to a geopandas dataframe, based on
-	coordinated in the regular pandas dataframe.
+def convert_to_geodf(df, proj, long_col, lat_col):
+    '''
+    Converts a regular pandas dataframe to a geopandas dataframe, based on
+    coordinated in the regular pandas dataframe.
 
-	Inputs:
-		df (Pandas DataFrame): the dataframe to convert
+    Inputs:
+        df (Pandas DataFrame): the dataframe to convert
         proj (dict): the sprojection for the GeoDataFrame coordinates
+        long_col (str): the name of the column containing longitude coordinates
+        lat_col (str): the name of the column containing latitude coordinates
 
-	Returns (geopandas GeoDataFrame)
-	'''
-	df['coordinates'] = df.apply(lambda x: (x.xcoordinate, x.ycoordinate), axis=1)
-	df = df[~df.xcoordinate.isna() & ~ df.ycoordinate.isna()].copy()
-	df.loc[:,'coordinates'] = df['coordinates'].apply(shapely.geometry.Point)
-	
-	geodf = geo_pd.GeoDataFrame(df, geometry='coordinates')
+    Returns (geopandas GeoDataFrame)
+    '''
+    df['coordinates'] = df.apply(lambda x: (x[long_col], x[lat_col]), axis=1)
+    df = df[df[long_col].notna() & df[lat_col].notna()]
+    df.loc[:,'coordinates'] = df['coordinates'].apply(shapely.geometry.Point)
+    print('here')
+    geodf = geo_pd.GeoDataFrame(df, geometry='coordinates')
     geodf.crs = proj
-	return geodf
+    return geodf
 
 def find_neighborhoods(geo_df, neighborhoods):
     '''
@@ -48,5 +50,5 @@ def find_neighborhoods(geo_df, neighborhoods):
 
     Returns: (GeoPandas GeoDataFrames)
     '''
-    geo_df.to_crs{neighborhoods.crs}
+    geo_df.to_crs(neighborhoods.crs)
 
