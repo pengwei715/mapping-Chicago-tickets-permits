6afc98ba74ca43b6d51962d8dae308ae445c997a
Ben Fogarty <benfogarty@cs.uchicago.edu>
2019-03-09 22:21:57 -0600

    Update shrink_tickets.py to fix bugs

--- ticket_project/shrink_tickets.py
+++ ticket_project/shrink_tickets.py
@@ -1,27 +1,30 @@
 import sys
 import pandas as pd
+import csv
 
 def go(input_path, output_path):
     '''
     Takes the full ProPublica parking tickets dataset as a CSV, reads in columns of
     interest, filters out rows not covered by permits data, and outputs the
-    reduced dataset as a CSV file.
+    reduced dataset and a map from violation descripitons to violation codes
+    as a CSV file.
 
     Inputs:
         input_path (string): the path to the full dataset
-        output_path (string): the path for the reduced dataset to be created in
+        output_path (string): the folder for the reduced dataset and violation
+            code dictionary to be created in
     '''
     cols = ['ticket_number', 'issue_date', 'violation_location',
             'zipcode', 'violation_code', 'violation_description',
-            'total_payments', 'address']
+            'geocoded_lng', 'geocoded_lat']
     col_types = {'ticket_number': str, 
                  'issue_date': str,
                  'violation_location': str,
                  'zipcode': 'category', 
-                 'violation_code': 'category',
-                 'violation_description': 'category',
-                 'total_payments': float,
-                 'address': str}
+                 'violation_code': str,
+                 'violation_description': str,
+                 'geocoded_lng': float,
+                 'gecoded_lat': float}
 
     print('Reading in full dataset...')
     df = pd.read_csv(input_path, usecols=cols,
@@ -33,19 +36,17 @@ def go(input_path, output_path):
     mask = df['issue_date'] > time_before
     df = df[mask]
 
-    print('Collapsing violation columns')
-    violation_dict = {}
-    for name, grouped_df in df.groupby(['violation_description']):
-        violation_dict[name] = grouped_df['violation_code'].iloc[1]
-    code_path = output_path[:-4] + '_coded_violations.csv'
-    with open(code_path, 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        for key, value in violation_dict.items():
-            writer.writerow([key, value])
+    print('Collapsing violation columns...')
+    with open(output_path + 'violations_dict.csv', 'w') as f:
+        violation_codes = df.drop_duplicates(['violation_code',
+                                              'violation_description'])\
+                            .violation_description\
+                            .to_csv(f)
     df = df.drop(['violation_description'], axis=1)
 
     print('Outputting reduced dataset...')
-    df.to_csv(output_path)
+    with open(output_path + 'reduced_tickets.csv', 'w') as f:
+        df.to_csv(f)
 
 
 
@@ -53,4 +54,6 @@ if __name__ == "__main__":
     usage = "python3 shrink_tickets.py <path to dataset> <output path>"
     assert (len(sys.argv) == 3), "Input and output path required."
     _, input_path, output_path = sys.argv
+    if output_path[-1] != '/':
+        output_path += '/'
     go(input_path, output_path)
