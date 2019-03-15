32a00c36f24175e1422dcfae95be932c126b1624
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-15 13:59:38 -0500

    add linkage file

--- ticket_project/link_per_tit.py
+++ ticket_project/link_per_tit.py
@@ -22,6 +22,18 @@ def link_permits_tickets(tit_data, output_file ='per_tit_whole.csv'):
     pdb.set_trace()
 
     per_data = pd.read_csv(DATA_DIR + 'clean_whole_permits.csv')
+    
+
+    tit['STREETNAME'] = tit['violation_location'].apply(lambda m: get_name(m).group(3)
+        if get_name(m) else None)
+    tit['DIRECTION'] =  tit['violation_location'].apply(lambda m: get_name(m).group(2)
+        if get_name(m) else None)
+    tit['t_streetnum'] = tit['violation_location'].apply(lambda m: get_name(m).group(1)
+        if get_name(m) else None)
+    tit['t_date'] = tit['issue_date'].apply(lambda m: get_date(m).group(0)
+        if get_date(m) else None)
+
+    
 
     tit_data['street_name'] = tit_data['street_name'].astype(str)
     tit_data['street_dir'] = tit_data['street_dir'].astype(str)
@@ -40,4 +52,12 @@ def link_permits_tickets(tit_data, output_file ='per_tit_whole.csv'):
 
 
 def get_name(m):
-    temp = re.match(r'(\d))
+    temp = re.match(r'(\d+) ([A-Z]) (\w+)\s?(\w?)',m)
+    if temp:
+        return temp
+    return None 
+def get_date(m):
+    temp = re.match(r'(\d{4}-\d{2}-\d{2})',m)
+    if temp:
+        return temp
+    return None
