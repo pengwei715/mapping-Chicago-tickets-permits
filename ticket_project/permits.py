41e66a445b62a7de26fdc903a0e4214ac4e9fffa
Ben Fogarty <benfogarty@cs.uchicago.edu>
2019-03-15 11:38:13 -0500

    Started working on possible blocking to speed up merge

--- ticket_project/permits.py
+++ ticket_project/permits.py
@@ -10,78 +10,124 @@ Find proper way interface with API and load in ata
 #!/usr/bin/env python
 
 #requre the sodapy library
-import os
 import sys
 import pandas as pd
 import requests
 import json
 import csv
 from sodapy import Socrata
-
+import re
+from datetime import datetime
+import dask.dataframe as dd
 
 DATA_DIR = "./data/"
+TIT_DIR = DATA_DIR + '/tickets'
 
-def get_sample(num, filename = "permits_sample.csv"):
+COLUMNS = ['uniquekey',
+           'applicationtype',
+           'applicationstartdate',
+           'worktype', 
+           'worktypedescription',
+           'applicationfinalizeddate',
+           'xcoordinate', 
+           'ycoordinate', 
+           'latitude',
+           'longitude',
+           'location',
+           'streetclosure',
+           'streetnumberfrom',
+           'streetnumberto',
+           'direction',
+           'streetname']
+
+def get_permits():
     '''
     Get some sample permit data
     Input:
         num: number of rows 
         filename: the name of the csv file 
     Return:
-        No return, the csv file contains the sample data.
+        Pandas dataframe that contains all the data of permits.
     '''
-    client = Socrata("data.cityofchicago.org", None)
-    if num == -1:
-        num = None
-    results = client.get("erhc-fkv9", limit=num)
-    write_csv(results, filename)
+    client = Socrata('data.cityofchicago.org',
+                 'SB7994tcuBpSSczrQvMx9N0Uy',
+                 username="benfogarty@uchicago.edu",
+                 password="d5Nut6LrCHL&")
+    results = client.get("erhc-fkv9",limit=1041814)
+    df = pd.DataFrame.from_records(results)
+    return df
 
-def trim_data(input_filename, output_filename):
-    '''
-    Find trim the data keep the time and location infor
-    Input:
-        input_filename: the raw csv file
-        output_filename: name of the result file
-    Return:
-        No return, filter the data and store it to csv file
-    '''
-    raw = read_csv(input_filename)
-    useful = raw.loc[:,['uniquekey','applicationtype',
-        'applicationtype','applicationstartdate',
-        'worktype', 'worktypedescription',
-        'applicationfinalizeddate','parkingmeterpostingorbagging',
-        'xcoordinate', 'ycoordinate', 'latitude',
-        'longitude','location','streetclosure',
-        'streetnumberfrom','streetnumberto',
-        'direction','streetname']]
-    output = DATA_DIR + output_filename
-    useful.to_csv(output)
+def clean_permits(df):
+    raw = dd.from_pandas(df, npartitions = 104)
+    raw = raw.loc[:,COLUMNS]
+    raw = raw[raw['streetclosure'].notna()]
+    raw = raw[raw['streetclosure']!='None']   
+    raw = raw.dropna(subset=['applicationstartdate', 
+        'applicationfinalizeddate','streetnumberfrom', 'streetnumberto'])
+    for item in ['applicationstartdate', 'applicationfinalizeddate']:
+        raw[item] = dd.to_datetime(per[item])   
+    return raw.compute()
 
+def split_tickets(inp='reduced_tickets.csv', out='tickets'): 
+
+  
+    inp = DATA_DIR + inp
+    raw =  pd.read_csv(inp, low_memory=False)
+    mask_2015 = raw['issue_date'].str.contains('2015',na =False)
+    mask_2016 = raw['issue_date'].str.contains('2016',na =False)
+    mask_2017 = raw['issue_date'].str.contains('2017',na =False)
+    mask_2018 = raw['issue_date'].str.contains('2018',na =False)
+
+    raw_2015 = raw[mask_2015]
+    raw_2015.to_csv(out + '_2015.csv')
+    raw_2016 = raw[mask_2016]
+    raw_2016.to_csv(out + '_2016.csv')
+    raw_2017 = raw[mask_2017]
+    raw_2017.to_csv(out + '_2017.csv')
+    raw_2018 = raw[mask_2018]
+    raw_2018.to_csv(out + '_2018.csv')
+    
+
+    result = [raw_2015, raw_2016, raw_2017, raw_2018]
+    whole = pd.concat(result)
+    whole.to_csv(DATA_DIR + out + '_15_18.csv')
+    return whole
+
+
+
+
+
+
+
+
+
+
+'''
 
 def read_csv(filename):
-    '''
     Read data from the file 
     Return pd dataframe
-    '''
-
     csv_file = DATA_DIR + filename
     data = pd.read_csv(csv_file)
     return data
 
-def write_csv(results,filename):
-
-
-    temp = set()
-    for i in range(200):
-        temp.update(set(results[i].keys()))
-    csv_columns = temp
+def write_csv(results, filename):
 
     csv_file = DATA_DIR + filename
     try:
         with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer = csv.DictWriter(csvfile, fieldnames=COLUMNS)
             writer.writeheader()
             for data in results:
                 writer.writerow(data)
     except IOError:
         print("I/O error")
+
+def select_by_year(filename, outputfile, year):
+    file = DATA_DIR + filename
+    raw = pd.read_csv(file)
+    mask = raw['applicationstartdate'].str.contains(str(year),na = False)
+    select = raw[mask]
+    csv_file = DATA_DIR + outputfile
+    select.to_csv(csv_file)
+'''
--- ticket_project/process_tickets.py
+++ ticket_project/process_tickets.py
@@ -2,6 +2,8 @@ import pandas as pd
 import csv
 import numpy
 import gc
+import neighborhoods as nbhds
+import geocoder
 
 
 def import_tickets(ticket_file, dictionary_file):
@@ -21,10 +23,10 @@ def import_tickets(ticket_file, dictionary_file):
                  'street_dir': 'category',
                  'street_name': 'category',
                  'zipcode': 'category',
-		         'geocoded_lng': float,
-		         'geocoded_lat': float}
+                 'geocoded_lng': float,
+                 'geocoded_lat': float}
     df = pd.read_csv(ticket_file, dtype=col_types, index_col='ticket_number',
-		             usecols=col_types.keys())
+                     usecols=col_types.keys())
     
     df['street_num'] = pd.to_numeric(df.street_num, downcast='unsigned')
     df['geocoded_lng'] = pd.to_numeric(df.geocoded_lng, downcast='float')
@@ -37,48 +39,94 @@ def import_tickets(ticket_file, dictionary_file):
     return df
 
 def generate_code_dict(filename):
-	'''
-	Reads in a csv of violation codes and violation names and
-	produces a dictionary which maps codes to names
-	'''
-	d = {}
-	with open(filename, mode='r') as file:
-		reader = csv.reader(file)
-		next(reader)
-		for key, value in reader:
-			d[key] = value
-	return d
+    '''
+    Reads in a csv of violation codes and violation names and
+    produces a dictionary which maps codes to names
+    '''
+    d = {}
+    with open(filename, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for key, value in reader:
+            d[key] = value
+    return d
+
+def link_with_neighborhoods(df):
+    nbhd = nbhds.import_geometries(nbhds.NEIGHS_ID)
+    geodf = nbhds.convert_to_geodf(df, 'geocoded_lng', 'geocoded_lat')
+
+    return nbhds.find_neighborhoods(geodf, nbhd)
 
 
 def search_for_patterns(df):
-	sweeping = df[ df['violation_code'] == 'STREET CLEANING'].sort_values('issue_date')
-	sweeping.groupby(sweeping['issue_date'].dt.month).count().issue_date
-	sweeping.groupby(sweeping['issue_date'].dt.hour).count().issue_date
+    sweeping = df[ df['violation_code'] == 'STREET CLEANING'].sort_values('issue_date')
+    sweeping.groupby(sweeping['issue_date'].dt.month).count().issue_date
+    sweeping.groupby(sweeping['issue_date'].dt.hour).count().issue_date
 
-	resampled = sweeping.set_index('issue_date').resample('Y')
+    resampled = sweeping.set_index('issue_date').resample('Y')
 
-def find_similar_tickets(tickets_df, date_range, violation_code, location):
-	'''
-	find_similar_tickets takes a set of parameters in the following form
-	and returns any tickets which match
+def filter_input(df, input_dict):
+    fail_str = '{} {} not found, ignoring'
+    success_str = 'filtered on {} number of tickets reduced from {} to {}'
+    column_dict = {'neighborhood': 'pri_neigh',
+                   'violation': 'violation_code',
+                   'start_date': 'issue_date',
+                   'end_date': 'issue_date',
+                   'location': ['geocoded_lng', 'geocoded_lat']}
+    dist_diff = 0.01
 
-	inputs: tickets_df (df): result of import_tickets
-			date_range (tuple): ('YYYY-MM-DD', 'YYYY-MM-DD')
-			violation_code (str): 'violation_code'
-			location (tuple): (lat, lon)
-	returns: df
-	'''
-	diff = 0.001
-		   
-	mask = (tickets_df['issue_date'] > date_range[0]) & \
-		   (tickets_df['issue_date'] < date_range[1]) & \
-		   (tickets_df['violation_code'] == violation_code) & \
-		   (tickets_df['geocoded_lat'] < location[0] + diff) & \
-		   (tickets_df['geocoded_lat'] > location[0] - diff) & \
-		   (tickets_df['geocoded_lng'] < location[1] + diff) & \
-		   (tickets_df['geocoded_lng'] > location[1] - diff)
-
-	return tickets_df[mask]
+    for key in input_dict:
+        row_nums = df.shape[0]
 
 
+        if key in ('neighborhood', 'violation'):
+            unique_vals = df[column_dict[key]].unique()
+            input_val = input_dict[key]
+            
+            if input_val not in unique_vals:
+                print(fail_str.format(key, input_val))
+            else:
+                df = df[ df[column_dict[key]] == input_val ]
+                print(success_str.format(input_val, row_nums, df.shape[0]))
+        if key in ('start_date', 'end_date'):
+            date_input = input_dict[key]
+            if key == 'start_date':
+                df = df[ df[column_dict[key]] > date_input]
+            else:
+                df = df[ df[column_dict[key]] < date_input]
+            print(success_str.format(date_input, row_nums, df.shape[0]))
+        if key == 'location':
+            g = geocoder.osm(input_dict[key])
+            input_val = input_dict[key]
+            if g.x and g.y:
+                mask = (df[column_dict[key][0]] < g.x + dist_diff) & \
+                       (df[column_dict[key][0]] > g.x - dist_diff) & \
+                       (df[column_dict[key][1]] < g.y + dist_diff) & \
+                       (df[column_dict[key][1]] > g.y - dist_diff)
 
+                df = df[mask]
+                print(success_str.format(input_val, row_nums, df.shape[0]))
+            else:
+                print(fail_str.format(key, input_val))
+    return df
+
+
+
+def find_similar_tickets(tickets_df, input_dict):
+    '''
+    find_similar_tickets takes a set of parameters in the following form
+    and returns any tickets which match
+
+    inputs: tickets_df (df): result of import_tickets
+            input_dict (dict): a dictionary which includes various filters
+                               including...
+                               'neighborhood' : 'neighborhood'
+                               'start_date' : 'YYYY-MM_DD'
+                               'end_date' : 'YYYY-MM_DD'
+                               'location' : ex. 'number street Chicago, IL'
+                               'violation' : 'violation_code'
+    returns: df
+    '''
+
+    new = filter_input(tickets_df, input_dict)
+    
