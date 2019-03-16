7df96b6eff1739a275bf15e5a28529e4b3b1e8bb
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-16 15:59:32 -0500

    get the related big table

--- ticket_project/link_per_tit.py
+++ ticket_project/link_per_tit.py
@@ -1,57 +1,54 @@
 '''
 link two dataframe together
+We only care about the violation_code 'PARKING/STANDING PROHIBITED ANYTIME'
+with full streetclosure during the permits time and in the permits area
 '''
-import re
+
 import pandas as pd
+import re
+from datetime import datetime
 import numpy as np
-import dask.dataframe as dd
 
-DATA_DIR = "./data/"
-
-def link_permits_tickets(per_data, tit_data, output_file='per_tit_whole.csv'):
+def link_permits_tickets(per, tik):
     '''
     Join two dataframe together based on the location and issue time
+    
     Input:
         output_file: filename of the big table
-
     Return:
         df: pandas dataframe contains the large joint table
     '''
+    get = lambda m: re.match(r'(.+) (.+)$',m).group(1).upper()\
+        if re.match(r'(.+) (.+)$',m) else None
 
-    abstract = lambda m: get_name(m).group(1).upper() if get_name(m) else None
+    per = per[per['streetclosure'] == 'Full']
+    tik = tik[tik['violation_code'] == 'PARKING/STANDING PROHIBITED ANYTIME']
+    tik['upper_streetname'] = tik['street_name'].apply(lambda m: get(m))
+    tik['upper_streetname'] = tik['upper_streetname'].astype('category')
 
-    tit_data['upper_streetname'] = tit_data['street_name'].apply(abstract)
-    tit_data['upper_streetname'] = tit_data['upper_streetname'].astype('category')
+    per = per.rename(index = str, columns = {'streetname': 'upper_streetname', 
+                                             'direction': 'street_dir'})
+    
+    per['streetnumberfrom'] = np.floor(per['streetnumberfrom']/100)*100
+    per['streetnumberto'] = np.ceil(per['streetnumberto']/100)*100
 
-    per_data = per_data.rename(index=str,\
-        columns={'streetname': 'upper_streetname', 'direction': 'street_dir'})
-
-    per_data['streetnumberfrom'] = np.floor(per_data['streetnumberfrom']/100) * 100
-    per_data['streetnumberto'] = np.ceil(per_data['streetnumberto']/100) * 100
-
-    tit_data = dd.from_pandas(tit_data, npartitions=100)
-    per_data = dd.from_pandas(per_data, npartitions=40)
-
-    combo = dd.merge(per_data, tit_data, on=['upper_streetname', 'street_dir'])
-    combo = combo[combo['streetnumberfrom'] <= combo['street_num']]
-    combo = combo[combo['streetnumberto'] >= combo['street_num']]
-    combo = combo[combo['issue_date'] >= combo['applicationstartdate']]
-    combo = combo[combo['issue_date'] <= combo['applicationfinalizeddate']]
-
-    combo = combo.compute()
-    combo.to_csv(DATA_DIR + output_file)
-    print('done')
+    combo = tik.merge(per, on = ['upper_streetname','street_dir'])
+    combo= combo[combo['streetnumberfrom'] <= combo['street_num']]
+    combo= combo[combo['streetnumberto'] >= combo['street_num']]
+    combo= combo[combo['issue_date'] >= combo['applicationstartdate']]
+    combo= combo[combo['issue_date'] <= combo['applicationfinalizeddate']]
 
     return combo
 
-def get_name(string):
-    '''
-    Helper function to get the streetname
 
-    Input:
-        m: street_name string from tickets
-    Return:
-        match object
+def top_5(combo):
     '''
-    match = re.match(r'(.+) (.+)$', string)
-    return match if match else None
+    Get the top 5 permits worktype lead to tickets
+    Input:
+        combo: dataframe of the linkage data
+    Return:
+        list of string 
+    '''
+    top_5 = combo.groupby('worktypedescription').count().reset_index()
+    top_5 =
+    return list(top_5['worktypedescription'])
