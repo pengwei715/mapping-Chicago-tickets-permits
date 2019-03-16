f512ca75abd96974cb268cff24467b01160b46c1
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-15 20:18:49 -0500

    pylinted this file

--- ticket_project/link_per_tit.py
+++ ticket_project/link_per_tit.py
@@ -1,18 +1,14 @@
 '''
 link two dataframe together
 '''
-import pandas as pd
-import csv
 import re
-from datetime import datetime
-import process_tickets as pro
-import permits as per
-import dask.dataframe as dd
-import pdb
+import pandas as pd
 import numpy as np
+import dask.dataframe as dd
+
 DATA_DIR = "./data/"
 
-def link_permits_tickets(per_data, tit_data, output_file ='per_tit_whole.csv'):
+def link_permits_tickets(per_data, tit_data, output_file='per_tit_whole.csv'):
     '''
     Join two dataframe together based on the location and issue time
     Input:
@@ -22,40 +18,40 @@ def link_permits_tickets(per_data, tit_data, output_file ='per_tit_whole.csv'):
         df: pandas dataframe contains the large joint table
     '''
 
-    #per_data = pd.read_csv(DATA_DIR + 'clean_whole_permits.csv')
+    abstract = lambda m: get_name(m).group(1).upper() if get_name(m) else None
 
-
-    tit_data['upper_streetname'] = tit_data['street_name'].apply(lambda m: get_name(m).group(1).upper()
-        if get_name(m) else None)
+    tit_data['upper_streetname'] = tit_data['street_name'].apply(abstract)
     tit_data['upper_streetname'] = tit_data['upper_streetname'].astype('category')
-    print(tit_data.info(memory_usage='deep'))
-    #tit_data['street_name'] = tit_data['street_name'].astype(str)
-    #tit_data['street_dir'] = tit_data['street_dir'].astype(str)
 
-    per_data = per_data.rename(index = str, columns = {'streetname': 'upper_streetname', 'direction': 'street_dir'})
+    per_data = per_data.rename(index=str,\
+        columns={'streetname': 'upper_streetname', 'direction': 'street_dir'})
+
     per_data['streetnumberfrom'] = np.floor(per_data['streetnumberfrom']/100) * 100
     per_data['streetnumberto'] = np.ceil(per_data['streetnumberto']/100) * 100
 
-    tit_data = dd.from_pandas(tit_data, npartitions = 100)
-    per_data = dd.from_pandas(per_data, npartitions = 40)
-    combo = dd.merge(per_data, tit_data, on = ['upper_streetname','street_dir'])
-    combo= combo[combo['streetnumberfrom'] <= combo['street_num']]
-    combo= combo[combo['streetnumberto'] >= combo['street_num']]
-    combo= combo[combo['issue_date'] >= combo['applicationstartdate']]
-    combo= combo[combo['issue_date'] <= combo['applicationfinalizeddate']]
+    tit_data = dd.from_pandas(tit_data, npartitions=100)
+    per_data = dd.from_pandas(per_data, npartitions=40)
+
+    combo = dd.merge(per_data, tit_data, on=['upper_streetname', 'street_dir'])
+    combo = combo[combo['streetnumberfrom'] <= combo['street_num']]
+    combo = combo[combo['streetnumberto'] >= combo['street_num']]
+    combo = combo[combo['issue_date'] >= combo['applicationstartdate']]
+    combo = combo[combo['issue_date'] <= combo['applicationfinalizeddate']]
 
     combo = combo.compute()
-    pdb.set_trace()
     combo.to_csv(DATA_DIR + output_file)
-
     print('done')
 
     return combo
 
+def get_name(string):
+    '''
+    Helper function to get the streetname
 
-def get_name(m):
-    #pdb.set_trace()
-    temp = re.match(r'(.+) (.+)$',m)
-    if temp:
-        return temp
-    return None
+    Input:
+        m: street_name string from tickets
+    Return:
+        match object
+    '''
+    match = re.match(r'(.+) (.+)$', string)
+    return match if match else None
