721428565a26820c148d1f85cc7a3744312b2562
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-15 21:21:07 -0500

    pylinted the file

--- ticket_project/permits.py
+++ ticket_project/permits.py
@@ -1,23 +1,9 @@
 '''
-Interface of Permits:
-https://data.cityofchicago.org/Transportation/Transportation-Department-Permits/pubx-yq2d/data
-Identify approved permits, 
-permits that actually resulted in street closures, 
-trim data to eliminate unnecessary columns
-Find proper way interface with API and load in ata
+Get the permit data from chicago data portal
+Clean the data
 '''
-
-#!/usr/bin/env python
-
-#requre the sodapy library
-import sys
 import pandas as pd
-import requests
-import json
-import csv
 from sodapy import Socrata
-import re
-from datetime import datetime as dt
 import dask.dataframe as dd
 import pdb
 
@@ -25,9 +11,8 @@ DATA_DIR = "./data/"
 TIT_DIR = DATA_DIR + 'tickets/'
 
 COLUMNS = ['uniquekey',
-           'applicationtype',
            'applicationstartdate',
-           'worktype', 
+           'worktype',
            'worktypedescription',
            'applicationfinalizeddate',
            'latitude',
@@ -40,43 +25,47 @@ COLUMNS = ['uniquekey',
 
 MAXSIZE = 1041814
 
-def get_permits():
+def get_permits(num=MAXSIZE):
     '''
     Get some sample permit data
     Input:
-        num: number of rows 
-        filename: the name of the csv file 
+        num: number of rows
+        filename: the name of the csv file
     Return:
         Pandas dataframe that contains all the data of permits.
     '''
     client = Socrata('data.cityofchicago.org',
-                 'SB7994tcuBpSSczrQvMx9N0Uy',
-                 username="benfogarty@uchicago.edu",
-                 password="d5Nut6LrCHL&")
-    results = client.get("erhc-fkv9",limit=MAXSIZE)
-    df = pd.DataFrame.from_records(results)
-    return df
+    	             'SB7994tcuBpSSczrQvMx9N0Uy',
+                     username="benfogarty@uchicago.edu",
+                     password="d5Nut6LrCHL&")
+
+    results = client.get("erhc-fkv9", limit=num)
+    return pd.DataFrame.from_records(results)
 
 def clean_permits():
     '''
     Clean the permits data
-    Input:
-        df: pandas dataframe get from web
     Return:
-        cleaned dataframe 
+        cleaned dataframe
     '''
-    df = get_permits()
-    raw = dd.from_pandas(df, npartitions = 104)
-    raw = raw.loc[:,COLUMNS]
-      
-    raw = raw.dropna(subset=['applicationstartdate', 
-        'applicationfinalizeddate','streetnumberfrom', 'streetnumberto'])
+    data = get_permits()
+    raw = dd.from_pandas(data, npartitions=104)
+    raw = raw.loc[:, COLUMNS]
+    lst = ['applicationstartdate',
+           'applicationfinalizeddate',
+           'streetnumberfrom',
+           'streetnumberto']
+    raw = raw.dropna(subset=lst)
+
     for item in ['applicationstartdate', 'applicationfinalizeddate']:
         raw[item] = dd.to_datetime(raw[item])
+
     for item in ['streetnumberfrom', 'streetnumberto']:
-        raw[item] = raw[item].astype('int32') 
+        raw[item] = raw[item].astype('int32')
+
     clean = raw.compute()
     clean = clean[clean['streetclosure'].notna()]
-    clean = clean[clean['streetclosure']!='None']
-    clean = clean[clean['streetclosure']!='NA']
+    clean = clean[clean['streetclosure'] != 'None']
+    clean = clean[clean['streetclosure'] != 'NA']
+
     return clean
