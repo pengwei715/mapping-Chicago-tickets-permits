b3082c759baf3bc24d28496a93925326e4a09ca9
Ben Fogarty <benfogarty@cs.uchicago.edu>
2019-03-17 15:05:06 -0500

    killed bug that was deleting records that should be linked!!!!

--- ticket_project/link_per_tit.py
+++ ticket_project/link_per_tit.py
@@ -1,6 +1,6 @@
 '''
 link two dataframe together
-We only care about the violation_code 
+We only care about the violation_code 'PARKING/STANDING PROHIBITED ANYTIME'
 with full streetclosure during the permits time and in the permits area
 '''
 
@@ -20,18 +20,10 @@ def link_permits_tickets(per, tik):
         combo: pandas dataframe contains the large joint table
     '''
     per = per[per['streetclosure'] == 'Full']
-    type_lst = ['NO STANDING/PARKING TIME RESTRICTED',
-                'PARKING/STANDING PROHIBITED ANYTIME' ]
-    tik = tik[tik['violation_code'].isin(type_lst) ]
-    cutoff = datetime.strptime('07-13-2015', '%m-%d-%Y')
-    #tik = tik[tik['violation_code']=='PARKING/STANDING PROHIBITED ANYTIME']
-    tik = tik[tik.issue_date >= cutoff]
-    tik['upper_streetname'] = tik.street_name.str.extract(r'(.+)\s\S+\Z',
-                                                          expand=True)
+    tik = tik[tik['violation_code'] == 'NO STANDING/PARKING TIME RESTRICTED']
+    tik['upper_streetname'] = tik.street_name.str.extract(r'(.+)\s.+\Z')
     tik['upper_streetname'] = tik.upper_streetname.str.upper()
     tik['upper_streetname'] = tik['upper_streetname'].astype('category')
-    print(len(tik))
-    print(len(per))
 
     per = per.rename(index=str, columns = {'streetname': 'upper_streetname', 
                                              'direction': 'street_dir'})
@@ -40,12 +32,14 @@ def link_permits_tickets(per, tik):
     per.loc[:,'streetnumberto'] = np.ceil(per['streetnumberto']/100)*100
 
     tik = tik.reset_index()
-    combo = tik.merge(per, on = ['upper_streetname','street_dir'])\
-               .drop_duplicates('ticket_number')
+    combo = tik.merge(per, on =['upper_streetname','street_dir'])
+
     combo= combo[combo['streetnumberfrom'] <= combo['street_num']]
+
     combo= combo[combo['streetnumberto'] >= combo['street_num']]
     combo= combo[combo['issue_date'] >= combo['applicationstartdate']]
     combo= combo[combo['issue_date'] <= combo['applicationexpiredate']]
+    combo = combo.drop_duplicates('ticket_number')
     #combo.to_csv('result.csv')
 
     return combo
