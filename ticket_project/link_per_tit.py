15e687835f763da2363ece50648bde7a708829e5
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-16 20:33:56 -0500

    change a little

--- ticket_project/link_per_tit.py
+++ ticket_project/link_per_tit.py
@@ -8,15 +8,17 @@ import pandas as pd
 import re
 from datetime import datetime
 import numpy as np
+import pdb
 
 def link_permits_tickets(per, tik):
     '''
     Join two dataframe together based on the location and issue time
     
     Input:
-        output_file: filename of the big table
+        per: data frame of permits data
+        tik: data frame of tickets data
     Return:
-        df: pandas dataframe contains the large joint table
+        combo: pandas dataframe contains the large joint table
     '''
     get = lambda m: re.match(r'(.+) (.+)$',m).group(1).upper()\
         if re.match(r'(.+) (.+)$',m) else None
@@ -26,61 +28,18 @@ def link_permits_tickets(per, tik):
     tik['upper_streetname'] = tik['street_name'].apply(lambda m: get(m))
     tik['upper_streetname'] = tik['upper_streetname'].astype('category')
 
-    per = per.rename(index = str, columns = {'streetname': 'upper_streetname', 
+    per = per.rename(index=str, columns = {'streetname': 'upper_streetname', 
                                              'direction': 'street_dir'})
     
     per['streetnumberfrom'] = np.floor(per['streetnumberfrom']/100)*100
     per['streetnumberto'] = np.ceil(per['streetnumberto']/100)*100
-
-    tik = tik.reset_index()
-    combo = tik.merge(per, on = ['upper_streetname','street_dir'])
-               .drop_duplicates('index')
+    
+    combo = tik.merge(per, on = ['upper_streetname','street_dir'])\
+               .drop_duplicates('ticket_number')
     combo= combo[combo['streetnumberfrom'] <= combo['street_num']]
     combo= combo[combo['streetnumberto'] >= combo['street_num']]
     combo= combo[combo['issue_date'] >= combo['applicationstartdate']]
     combo= combo[combo['issue_date'] <= combo['applicationexpiredate']]
-    combo.to_csv('result.csv')
-    return combo
-
-'''
-link two dataframe together
-We only care about the violation_code 'PARKING/STANDING PROHIBITED ANYTIME'
-with full streetclosure during the permits time and in the permits area
-'''
-
-import pandas as pd
-import re
-from datetime import datetime
-import numpy as np
-
-def link_permits_tickets(per, tik):
-    '''
-    Join two dataframe together based on the location and issue time
-    
-    Input:
-        output_file: filename of the big table
-    Return:
-        df: pandas dataframe contains the large joint table
-    '''
-    get = lambda m: re.match(r'(.+) (.+)$',m).group(1).upper()\
-        if re.match(r'(.+) (.+)$',m) else None
-
-    per = per[per['streetclosure'] == 'Full']
-    tik = tik[tik['violation_code'] == 'NO STANDING/PARKING TIME RESTRICTED']
-    tik['upper_streetname'] = tik['street_name'].apply(lambda m: get(m))
-    tik['upper_streetname'] = tik['upper_streetname'].astype('category')
-
-    per = per.rename(index = str, columns = {'streetname': 'upper_streetname', 
-                                             'direction': 'street_dir'})
-    
-    per['streetnumberfrom'] = np.floor(per['streetnumberfrom']/100)*100
-    per['streetnumberto'] = np.ceil(per['streetnumberto']/100)*100
-
-    combo = tik.merge(per, on = ['upper_streetname','street_dir'])
-    combo= combo[combo['streetnumberfrom'] <= combo['street_num']]
-    combo= combo[combo['streetnumberto'] >= combo['street_num']]
-    combo= combo[combo['issue_date'] >= combo['applicationstartdate']]
-    combo= combo[combo['issue_date'] <= combo['applicationenddate']]
     combo.to_csv('result.csv')
     return combo
 
