bad18addac111b4506b9b7eacebf95555c6515b7
Eric Langowski <langowski@cs.uchicago.edu>
2019-03-17 19:55:16 -0500

    test

--- ticket_project/shared_fct.py
+++ ticket_project/shared_fct.py
@@ -8,6 +8,8 @@ import pandas as pd
 import re
 from datetime import datetime
 import numpy as np
+import neighborhoods as nbhds
+
 
 def link_permits_tickets(per, tik):
     '''
@@ -43,3 +45,87 @@ def link_permits_tickets(per, tik):
     #combo.to_csv('result.csv')
 
     return combo
+
+
+
+def link_with_neighborhoods(df, lng_col, lat_col):
+    '''
+    link_with_neighborhoods is a helper function which processes a dataframe
+    and returns a geocoded one
+    input: (df) tickets_df
+    output: (geo_df) geo_tickets_df
+    '''
+    nbhd = nbhds.import_geometries(nbhds.NEIGHS_ID)
+    geodf = nbhds.convert_to_geodf(df, lng_col, lat_col)
+    return nbhds.find_neighborhoods(geodf, nbhd)
+
+
+def generate_code_dict(filename):
+    '''
+    Reads in a csv of violation codes and violation names and
+    produces a dictionary which maps codes to names
+    '''
+    d = {}
+    with open(filename, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for key, value in reader:
+            d[key] = value
+    return d
+
+def filter_input(df, input_dict, column_dict, db_type):
+    '''
+    filter_input takes a tickets_df and returns a filtered df based on the
+    filters given in input_dict
+
+    inputs: (df) tickets_df
+            (dict) input_dict
+    returns: (df) filtered df
+    '''
+    fail_str = '{} {} not found, ignoring'
+    success_str = 'filtered on {} number of ' + db_type + ' reduced from {} to {}'
+#    column_dict = {'violation': 'violation_code',
+#                   'start_date': 'issue_date',
+#                   'end_date': 'issue_date',
+#                   'location': ['geocoded_lng', 'geocoded_lat'],
+#                   'neighborhood': 'zipcode'}
+    dist_diff = 0.0145 #approximately 1 mile in distance
+
+    for key, val in input_dict.items():
+        row_nums = df.shape[0]
+
+        if key == {'worktype', 'closing_type', 'streetname', 'violation'}:
+            unique_vals = df[column_dict[key]].unique()
+            if val not in unique_vals:
+                print(fail_str.format(key, val))
+            else:
+                df = df[df[column_dict[key]] == val]
+                print(success_str.format(val, row_nums, df.shape[0]))
+        if key == 'neighborhood':
+            neigh_dict = nbhds.link_neighs_zips()
+            if val in neigh_dict:
+                df = df[df[column_dict[key]].isin(neigh_dict[val])]
+                print(success_str.format('zipcode approximation for ' + val, \
+                                         row_nums, df.shape[0]))
+            else:
+                print(fail_str.format(key, val))
+        if key in ('start_date', 'end_date'):
+            if key == 'start_date':
+                df = df[df[column_dict[key]] > val]
+            else:
+                df = df[df[column_dict[key]] < val]
+            print(success_str.format(val, row_nums, df.shape[0]))
+        if key == 'location':
+            g = geocoder.osm(input_dict[key])
+            if g.x and g.y:
+                mask = (df[column_dict[key][0]] < g.x + dist_diff) & \
+                       (df[column_dict[key][0]] > g.x - dist_diff) & \
+                       (df[column_dict[key][1]] < g.y + dist_diff) & \
+                       (df[column_dict[key][1]] > g.y - dist_diff)
+                df = df[mask]
+                print(success_str.format(\
+                    'locations within approxmiately one mile of ' + val, \
+                    row_nums, df.shape[0]))
+            else:
+                print(fail_str.format(key, val))
+    return df
