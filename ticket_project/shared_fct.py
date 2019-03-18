9629bc7b11c875a2c401ea3d221e513bc9db452d
Peng Wei <pengwei@cs.uchicago.edu>
2019-03-18 12:38:52 -0500

    7.76 from pylint

--- ticket_project/shared_fct.py
+++ ticket_project/shared_fct.py
@@ -1,7 +1,5 @@
 '''
-link two dataframe together
-We only care about the violation_code 
-with full streetclosure during the permits time and in the permits area
+Common functions of the whole software
 '''
 
 import pandas as pd
@@ -18,79 +16,76 @@ import json
 import sys
 import csv
 
-
+#columns of tickets data that users can filter
 TICKET_COLUMNS = {'violation': 'violation_code',
-                   'start_date': 'issue_date',
-                   'end_date': 'issue_date',
-                   'location': ['geocoded_lng', 'geocoded_lat'],
-                   'neighborhood': 'zipcode'}
+                  'start_date': 'issue_date',
+                  'end_date': 'issue_date',
+                  'location': ['geocoded_lng', 'geocoded_lat'],
+                  'neighborhood': 'zipcode'}
+#columns of permits data that users can filter
 PERMIT_COLUMNS = {'worktype': 'worktypedescription',
-                    'start_date': 'applicationexpiredate',
-                    'end_date': 'applicationexpiredate',
-                    'location': ['longitude', 'latitude'],
-                    'closing_type': 'streetclosure'}
+                  'start_date': 'applicationexpiredate',
+                  'end_date': 'applicationexpiredate',
+                  'location': ['longitude', 'latitude'],
+                  'closing_type': 'streetclosure'}
 
 def link_permits_tickets(per, tik1):
     '''
+    Filter the tickets on two violation type then
     Join two dataframe together based on the location and issue time
-    
+
     Input:
-        per: data frame of permits data
-        tik: data frame of tickets data
+        per: dataframe of permits data
+        tik: dataframe of tickets data
     Return:
         combo: pandas dataframe contains the large joint table
     '''
     per = per[per['streetclosure'] == 'Full']
     tik = tik1[tik1['violation_code'].isin['NO STANDING/PARKING TIME RESTRICTED',
                                            'PARKING/STANDING PROHIBITED ANYTIME']]
-    
     tik['upper_streetname'] = tik.street_name.str.extract(r'(.+)\s.+\Z')
     tik['upper_streetname'] = tik.upper_streetname.str.upper()
     tik['upper_streetname'] = tik['upper_streetname'].astype('category')
 
-    per = per.rename(index=str, columns = {'streetname': 'upper_streetname', 
-                                             'direction': 'street_dir'})
-    
-    per.loc[:,'streetnumberfrom'] = np.floor(per['streetnumberfrom']/100)*100
-    per.loc[:,'streetnumberto'] = np.ceil(per['streetnumberto']/100)*100
+    per = per.rename(index=str, columns={'streetname': 'upper_streetname',
+                                         'direction': 'street_dir'})
+
+    per.loc[:, 'streetnumberfrom'] = np.floor(per['streetnumberfrom']/100)*100
+    per.loc[:, 'streetnumberto'] = np.ceil(per['streetnumberto']/100)*100
 
     tik = tik.reset_index()
-    combo = tik.merge(per, on =['upper_streetname','street_dir'])
+    combo = tik.merge(per, on=['upper_streetname', 'street_dir'])
 
-    combo= combo[combo['streetnumberfrom'] <= combo['street_num']]
-
-    combo= combo[combo['streetnumberto'] >= combo['street_num']]
-    combo= combo[combo['issue_date'] >= combo['applicationstartdate']]
-    combo= combo[combo['issue_date'] <= combo['applicationexpiredate']]
+    combo = combo[combo['streetnumberfrom'] <= combo['street_num']]
+    combo = combo[combo['streetnumberto'] >= combo['street_num']]
+    combo = combo[combo['issue_date'] >= combo['applicationstartdate']]
+    combo = combo[combo['issue_date'] <= combo['applicationexpiredate']]
     combo = combo.drop_duplicates('ticket_number')
-    #combo.to_csv('result.csv')
 
     return combo
 
-
-
 def link_with_neighborhoods(df, lng_col, lat_col):
     '''
-    link_with_neighborhoods is a helper function which processes a dataframe
-    and returns a geocoded one
-    input: (df) tickets_df
-    output: (geo_df) geo_tickets_df
+    Helper function to get a geocoded dataframe
+
+    Input:
+        df: dataframe with latitude and longtitude columns
+    Return:
+        A dataframe that is geocoded
     '''
     nbhd = nbhds.import_geometries(nbhds.NEIGHS_ID)
     geodf = nbhds.convert_to_geodf(df, lng_col, lat_col)
     return nbhds.find_neighborhoods(geodf, nbhd)
 
-
-
-
 def filter_input(df, input_dict, column_dict, db_type):
     '''
-    filter_input takes a tickets_df and returns a filtered df based on the
-    filters given in input_dict
+    Filter the data frame based on the input_dict
 
-    inputs: (df) tickets_df
-            (dict) input_dict
-    returns: (df) filtered df
+    Inputs:
+        df: data frame to filter
+        input_dict: dict of the user's input
+    Returns:
+        df: filtered df
     '''
     fail_str = '{} {} not found, ignoring'
     success_str = 'filtered on {} number of ' + db_type + ' reduced from {} to {}'
@@ -140,7 +135,7 @@ def project_onto_chicago(geodf, nbhd, location_bool, db_type, neighborhood=""):
     project_onto_chicago takes a set of parameters in the following form
     and returns any tickets which match
 
-    inputs: geodf (geodf): geopandas df
+    Inputs: geodf (geodf): geopandas df
             nbhd (geodf): geopandas df
             location_bool (bool): True if location filters are present
             db_type (str): database name
@@ -169,7 +164,7 @@ def project_onto_chicago(geodf, nbhd, location_bool, db_type, neighborhood=""):
     else: #citywide
         fig, ax = plt.subplots(1)
         heat = geodf.dissolve(by='pri_neigh', aggfunc='count')
-        heat = nbhd.merge(heat, on='pri_neigh',how='left').fillna(0)
+        heat = nbhd.merge(heat, on='pri_neigh', how='left').fillna(0)
         heat.plot(ax=ax, cmap='coolwarm', column=first_col, linewidth=0.8,
                   linestyle='-')
         ax.axis('off')
@@ -177,7 +172,7 @@ def project_onto_chicago(geodf, nbhd, location_bool, db_type, neighborhood=""):
         n_min = min(heat[first_col])
         n_max = max(heat[first_col])
         leg = mpl.cm.ScalarMappable(cmap='coolwarm', norm=mpl.colors.Normalize(
-                                    vmin=n_min, vmax=n_max))
+            vmin=n_min, vmax=n_max))
         leg._A = []
         colorbar = fig.colorbar(leg)
 
@@ -200,8 +195,8 @@ def go_tickets(parameters):
         print('Error: Invalid parameter for tickets dataset!')
     else:
         print('Loading the tickets dataset...')
-        tickets = data_loader.import_tickets(data_loader.TICKETS_FILEPATH, 
-                                                 data_loader.VIOLATIONS_FILEPATH)
+        tickets = data_loader.import_tickets(data_loader.TICKETS_FILEPATH,
+                                             data_loader.VIOLATIONS_FILEPATH)
         #call filtering function
         tickets = filter_input(tickets, parameters, TICKET_COLUMNS, 'tickets')
         print('Generating analysis...')
@@ -211,7 +206,8 @@ def go_tickets(parameters):
     if 'location' in parameters or 'neighborhood' in parameters:
         location_bool = True
 
-    project_onto_chicago(tickets, nbhd, location_bool, 'tickets', parameters.get('neighborhood', ""))
+    project_onto_chicago(tickets, nbhd, location_bool, 'tickets',
+    	                 parameters.get('neighborhood', ""))
 
 
 def go_permits(parameters):
@@ -249,13 +245,14 @@ def go_linked(parameters):
         strings with parameter values
     '''
 
-    if set(parameters.keys()) - (set(PERMIT_COLUMNS.keys()) | set(TICKET_COLUMNS.keys())):
+    if set(parameters.keys()) - (set(PERMIT_COLUMNS.keys()) | 
+    	set(TICKET_COLUMNS.keys())):
         print('Error: Invalid parameter for linked dataset!')
     else:
         print('Loading the tickets dataset...')
         tickets = data_loader.import_tickets(data_loader.TICKETS_FILEPATH,
                                              data_loader.VIOLATIONS_FILEPATH)
-        tickets = filter_input(tickets, parameters, TICKET_COLUMNS, 'tickets')     
+        tickets = filter_input(tickets, parameters, TICKET_COLUMNS, 'tickets')   
 
         print('Loading the permits dataset...')
         pers = data_loader.get_permits('07-13-2015')
@@ -270,11 +267,13 @@ def go_linked(parameters):
 
 if __name__ == "__main__":
     usage = "python3 shrink_tickets.py <dataset> <parameters>"
-    assert (len(sys.argv) == 3), ('Please specify what data set to use',
-        '(tickets, permits, or links), and a JSON string specifying parameters')
+    assert (len(sys.argv) == 3), \
+           ('Please specify what data set to use',
+            '(tickets, permits, or links)', 
+            'and a JSON string specifying parameters')
     dataset = sys.argv[1]
-    assert dataset in ['tickets', 'permits', 'linked'], ('Must specify', 
-        'tickets or permits dataset in parameters.')
+    assert dataset in ['tickets', 'permits', 'linked'], \
+        ('Must specify', 'tickets or permits dataset in parameters.')
     parameters = json.loads(sys.argv[2])
     if dataset == 'tickets':
         go_tickets(parameters)
